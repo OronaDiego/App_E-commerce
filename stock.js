6fb70a08fc32b27d4f381379526265a161e782b7
Diego <diegosebastianorona@gmail.com>
2024-10-03 10:13:53 -0300

    agregando mas productos al stock

--- stock.js
+++ stock.js
@@ -1,5 +1,11 @@
 const baterias = [
     { marca: "Yamaha", precio: 500, stock: 0 },
     { marca: "Pearl", precio: 1000, stock: 5 },
-    { marca: "Tama", precio: 1500, stock: 10 }
+    { marca: "Tama", precio: 1500, stock: 10 },
+    { marca: "Ludwig", precio: 500, stock: 0 },
+    { marca: "Mapex", precio: 1000, stock: 5 },
+    { marca: "DW", precio: 1500, stock: 10 },
+    { marca: "Gretsch", precio: 500, stock: 0 },
+    { marca: "Sonor", precio: 1000, stock: 5 },
+    { marca: "PDP", precio: 1500, stock: 10 }
 ]
