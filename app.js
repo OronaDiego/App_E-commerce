4ce317101a2998c7a04ab4259ff9e04d7ccaee26
Diego <diegosebastianorona@gmail.com>
2024-10-03 22:20:39 -0300

    Ajustando el flujo

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 
-const carrito= []
+const carrito = []
 
-const listaOrdenada = ()=>{
-    const listaOrdenada = baterias.map(bata =>{
-        return `-${bata.marca} > Precio: $${bata.precio}`});
-        comprarBateria(listaOrdenada)
+const listaOrdenada = () => {
+    const listaOrdenada = baterias.map(bata => {
+        return `-${bata.marca} > Precio: $${bata.precio}`
+    });
+    comprarBateria(listaOrdenada)
 }
 
 //***************Ordeno la lista de menor a mayor*****/
 const ordXPrecioMenorMayor = () => {
-    baterias.sort((a,b)=> a.precio - b.precio);
+    baterias.sort((a, b) => a.precio - b.precio);
     listaOrdenada()
 }
 
@@ -31,66 +32,75 @@ const agregarBataCarrito = (bateriaMarca, bateriaPrecio, bataCantidad) => {
 
 alert(`Bienvenido a TiendaMusic`)
 
-const comprarBateria = (listaOrdenada)=>{
+const comprarBateria = (listaOrdenada) => {
     let volverAComprar;
     let bataElejida = '';
     let bataCantidad = 0;
 
-    do{
+    do {
         bataElejida = prompt(`Que bateria te gustaria comprar? \n${listaOrdenada.join('\n')}`);
+
         bataCantidad = parseInt(prompt(`Cuantas vas a comprar?`))
 
-        while((bataCantidad <= 0)|| isNaN(bataCantidad)){
+        while ((bataCantidad <= 0) || isNaN(bataCantidad)) {
             alert(`ATENCION!!! \nNo podes llevar ${bataCantidad} productos`)
             bataCantidad = parseInt(prompt(`Vuelve a ingresar la cantidad. \n"Recorda ingresar un valor numerico mayor a 0"`))
         }
 
-        const bata = baterias.find(b=> b.marca.toLowerCase() === bataElejida.toLowerCase())
+        const bata = baterias.find(b => b.marca.toLowerCase() === bataElejida.toLowerCase())
         //console.log(bata);
-        
-        if(bata){
-            alert(`${bataCantidad} Bateria ${bata.marca} Agregada al carrito`)
+
+        if (bata) {
+            alert(`${bataCantidad} unidades de Bateria ${bata.marca} agregada al carrito`)
             agregarBataCarrito(bata.marca, bata.precio, bataCantidad)
             //carrito.push(bata.marca, bata.precio, bataCantidad)
-        }else if(bata === undefined ){
+        } else if (bata === undefined) {
             alert(`ALERTA!! \nPor favor ingresa una bata para poder realizar la busqueda en nuestra base de datos \nEl dato ingresado: ${bataElejida} no se permite`)
-        }else{
+        } else {
             alert('Lo siento pero no podemos encontrar el producto elejido')
         }
 
         volverAComprar = confirm('Queres realizar otra compra?')
-    }while(volverAComprar)
+    } while (volverAComprar)
 
-        
+    if (bataElejida !== null) {
         confirmarCarrito()
+    }
 }
 
 
-const confirmarCarrito = ()=>{
-const listaCarrito = carrito.map( b =>{
-    return `- ${b.marca} -${b.cantidad}unidades`
-})
-const cant = carrito.map(c=> {
-    return `${c.cantidad}`
+const confirmarCarrito = () => {
+    const listaCarrito = carrito.map(b => {
+        return `* ${b.marca} Unidades: ${b.cantidad}`
+    })
+    console.log(listaCarrito);
+
+    const cant = carrito.map(c => {
+        return `${c.cantidad}`
     })
 
-const aceptar = confirm(`Confirmar compra: \n ${listaCarrito.join('\n')}\n Presiones ACEPTAR para continuar, sino CANCELAR para cancelar la compra`)
+    const canBatas = carrito.length;
+    console.log(canBatas);
 
-    if(aceptar){
+
+
+    const aceptar = confirm(`Confirmar compra: \n ${listaCarrito.join('\n')}\n Presione: \n->ACEPTAR para confirmar \n->CANCELAR para cancelar la compra`)
+
+    if (aceptar) {
         alert('Felicidades ya casi es tuyo')
-        if(cant > 3){
+        if ((canBatas > 2)|| (cant > 1)) {
             descuento()
-            }
-    }else{
+        }
+    } else {
         alert(`Gracias por su visita, guardaremos su ${listaCarrito.join('\n')}\ny lo tendremos listo para cuando vuelva`)
     }
 
 }
 
 
-const descuento = ()=>{
-        
-    const desc = carrito.map(d=>{
+const descuento = () => {
+
+    const desc = carrito.map(d => {
         return `20% ${d.precio * 0.80}`
     })
     alert(`Obtuvo un descuento del ${desc} por llevar esa cantidad de productos`)
